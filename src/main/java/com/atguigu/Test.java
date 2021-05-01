24224870e73bed06ed8f63250c609ac06e0540b3
fangbeixxx <hello_fangbei@163.com>
2021-05-01 16:03:55 +0800

    firefix第一次提交

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!!");
+        System.out.println("hello world!! shanghai");
     }
 }
