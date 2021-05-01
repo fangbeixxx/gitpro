71bec0208aeebc8e188bc0ed1c98791bc4584556
fangbeixxx <hello_fangbei@163.com>
2021-05-01 16:04:56 +0800

    第一次修改

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!! shanghai");
+        System.out.println("hello world!! shanghai songjiang");
     }
 }
